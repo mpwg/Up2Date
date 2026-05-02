00c9aea314ee7d4b3ce93fa921c9cfb46a4d5d5c
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2026-05-02 19:13:07 +0200

    Initial Commit

---
+++ Up2Date/Up2DateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Up2DateApp.swift
+//  Up2Date
+//
+//  Created by Matthias Wallner-Géhri on 02.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Up2DateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
